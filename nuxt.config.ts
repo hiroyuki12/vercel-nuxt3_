1e8e76544ef6262d6e2208330690c2bf0c80983c
hiroyuki12 <hiroyuki12@gmail.com>
2022-08-01 20:16:22 +0900

    Update nuxt.config.ts.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,22 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/pwa'
+  ],
+  /*
+   ** @nuxtjs/pwa Configuration
+   ** https://github.com/nuxt-community/pwa-module
+   */
+  manifest: {
+    name: 'demo',
+    lang: 'ja',
+    short_name: 'demo',
+    title: 'demo',
+    'og:title': 'demo',
+    description: 'demoサイトです',
+    'og:description': 'demoサイトです',
+    theme_color: '#212121',
+    background_color: '#212121'
+  },
 })
