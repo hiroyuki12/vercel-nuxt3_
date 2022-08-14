d641014354b4e5aac6bf68823cb31388f9f75e4f
hiroyuki12 <hiroyuki12@gmail.com>
2022-08-15 02:44:41 +0900

    Update index Qiita.

--- app.vue
+++ app.vue
@@ -4,8 +4,8 @@
 
 <script setup>
 import './assets/css/style.css'
-//import Home from './components/Qiita.vue'
-import Home from './components/YouTubeApi.vue'
+import Home from './components/Qiita.vue'
+//import Home from './components/YouTubeApi.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
